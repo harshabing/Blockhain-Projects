2cdacda9b4a97b0622cb8be36f4750c14f79f8f9
Sriharsha Bingi <harshabing@gmail.com>
2018-08-28 21:20:04 -0400

    Update reviewing.sol

--- Blockchain--Reviews- Ratings - Liking-- SmartContract/reviewing.sol
+++ Blockchain--Reviews- Ratings - Liking-- SmartContract/reviewing.sol
@@ -1,22 +1,25 @@
 pragma solidity ^0.4.18;
-pragma experimental ABIEncoderV2;
+//pragma experimental ABIEncoderV2;
 
 
 contract reviewing {
     
     
-    mapping(address => mapping(uint256 =>string)) public reviews;
-    mapping(address => mapping(uint256 =>uint8)) public reviewlike;
-    mapping(address=>mapping(string=>uint8)) likerlimit;
-    mapping(address => uint256) YourCounter;
+    mapping(address => mapping(uint256 =>string)) public reviews; // This is for storing the review w.r.t address and review count from that address
+    mapping(address => mapping(uint256 =>uint8)) public reviewlike; // This is for storing if a particular person has liked a comment for not using that person's account address and review count and liked or not(0 for not liked and 1 for liked)
+    mapping(address=>mapping(string=>uint8)) likerlimit; // An address can like a review only once. Storing if that address has already liked.
+    mapping(address => uint256) YourCounter; // Total reviews
+    mapping(uint256 =>string) searchreview; // Unique Id for each review.
+    mapping(uint256=>address)uniquereview; // Unique Id's w.r.t addresses.
     address owner;
+    uint256 countVar=0; 
     constructor() public {
         
         owner = msg.sender;
         
     }
     
-    
+    // function to write the review
    function WriteReview(string tell) public returns (uint256,address,bool) {
       address reviewer=msg.sender;
            
@@ -24,30 +27,38 @@ contract reviewing {
        
        reviews[reviewer][YourCounter[reviewer]] = tell;
        reviewlike[reviewer][YourCounter[reviewer]] =0;
+       countVar++;
+       searchreview[countVar] = tell;
+       uniquereview[countVar] = msg.sender;
        
        return (YourCounter[reviewer],reviewer,true);
       } 
    
    
+    // function to delete the review
    function RemoveReview(uint256 RemoveId,address RemoveAddress) public returns(bool) {
        
        if(msg.sender==owner) {
            
            delete reviews[RemoveAddress][RemoveId];
+            countVar--;
             return true;
        }
        return false;
    }
        
+    // function to see the review
      function SeeReview(address getting, uint256 __id) public view returns(string) {
          
          return(reviews[getting][__id]);
      }
      
+    // function to get total reviews
      function YourCount(address _add) public view returns(uint256) {
          return (YourCounter[_add]);
      }
      
+    // function to like the review
      function LikeReview(address likeadd,uint256 __id) public returns(string say){
          
          if(likerlimit[msg.sender][reviews[likeadd][__id]] ==0)  {
@@ -62,16 +73,25 @@ contract reviewing {
      }
      
      
-   /*  function SearchComment(string str) public returns(string) {
+        // function to search for the review
+         function SearchComment(string str) public view returns(uint256,address) {
          
          
          
-     } */
+            for(uint i=0;i<=countVar;i++) {
+              
+                if(keccak256(abi.encodePacked((searchreview[i]))) == keccak256(abi.encodePacked((str)))){
+                    return (i,uniquereview[i]);
+                }
+                
+                }
+               
+            }
+             
+         }
+         
      
      
      
-   }
+     
    
-   
-   
-    
